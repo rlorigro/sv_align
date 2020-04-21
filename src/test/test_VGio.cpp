0d40907fea9d726d1af106286ea045a26f0dc2e7
ryan <rlorigro@ucsc.edu>
2020-04-20 17:59:54 -0700

    Updated VGio test

--- src/test/test_VGio.cpp
+++ src/test/test_VGio.cpp
@@ -30,7 +30,7 @@ int main(){
     path project_directory = script_path.parent_path().parent_path().parent_path();
 
     // Get test VCF path
-    path relative_gam_path = "/data/HG00733_chr14_85915451_haps_vs_shasta_asm.gam";
+    path relative_gam_path = "/data/HG00733_chr14_85915451_haps_vs_shasta_asm.gam.raw.bgz";
     path absolute_gam_path = project_directory / relative_gam_path;
 
     ifstream datastream(absolute_gam_path);
@@ -38,8 +38,21 @@ int main(){
     for (vg::io::ProtobufIterator<Alignment> it(datastream); it.has_current(); it.advance()) {
         auto vo_parts = unvo(it.tell_group());
 
-        cerr << "Found item " << " at VO " << it.tell_group()
-             << " = " << vo_parts.first << ", " << vo_parts.second << '\n';
+        Alignment& alignment = *it;
+        cerr << "\nName: " << alignment.name() << '\n'
+            << "Map Quality: " << alignment.mapping_quality() << '\n'
+            << "Mapping Size: " << alignment.path().mapping_size() << '\n'
+            << "Has Path: " << alignment.has_path() << '\n'
+            << "Path Length: " << alignment.path().length() << '\n'
+            << "Virtual Offset: " << it.tell_group() << " = " << vo_parts.first << ", " << vo_parts.second << '\n';
+
+        for (auto& mapping: alignment.path().mapping()){
+            for (auto& edit: mapping.edit()) {
+                cerr << "\nfrom_length:\t" << edit.from_length() << '\n';
+                cerr << "to_length:\t" << edit.to_length() << '\n';
+                cerr << "sequence:\t" << edit.sequence() << '\n';
+            }
+        }
     }
 
     return 0;
